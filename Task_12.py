0bf21cceade810effc2414a56066419046353388
KateBilous <katherinebilous@gmail.com>
2017-08-01 23:20:53 +0300

    Task-12 modified

--- Task_12.py
+++ Task_12.py
@@ -1,21 +1,18 @@
 # Написать функцию, которая рассчитывает сумму всех цифр некоторого трехзначного числа,
 #  введенного пользователем в консоли, без использования операторов цикла. А теперь и без строк :)
-class Numbers:
-    @staticmethod
-    def input():
-        m = int(input("Введите трехзначное число : "))
-        return m
+
+a = int(input("Введите трехзначное число : "))
 
 
-def sum_of_numbers():
-    a = Numbers.input()
+def sum_of_numbers(a):
     d1 = a % 10
     a = a // 10
     d2 = a % 10
     a = a // 10
     d3 = a % 10
     sum_ = d1 + d2 + d3
+
     return sum_
 
 
-print("Сумма чисел равна ", sum_of_numbers())
+print("Сумма чисел равна ", sum_of_numbers(a))
